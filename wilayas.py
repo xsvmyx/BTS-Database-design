8c96251260e225b0bb410d9cc2d2ad853e200290
xsvmyx <125303992+xsvmyx@users.noreply.github.com>
2025-09-25 14:26:37 +0000

    Add files via upload

---
+++ wilayas.py
@@ -0,0 +1,74 @@
+# zones = [
+#     Zone(id=1,name="Nord-Ouest", latitude="35.676940", longitude="-0.634930"),  # Nord-Ouest
+#     Zone(id=2,name="Nord-Centre", latitude="36.470000", longitude="2.828000"),   # Nord-Centre
+#     Zone(id=3,name="Nord-Est", latitude="36.350000", longitude="6.600000"),   # Nord-Est
+#     Zone(id=4,name="Hauts Plateaux", latitude="33.750000", longitude="4.500000"),   # Hauts Plateaux (centré)
+#     Zone(id=5,name="Sud", latitude="26.500000", longitude="2.500000")    # Sud
+# ]
+
+
+
+
+# wilayas = [
+# Wilaya(id=1, name="ADRAR", latitude="27.8743", longitude="-0.2939", zone_id=5),
+# Wilaya(id=2, name="CHLEF", latitude="36.1653", longitude="1.3340", zone_id=1),
+# Wilaya(id=3, name="LAGHOUAT", latitude="33.8006", longitude="2.8809", zone_id=4),
+# Wilaya(id=4, name="OUM EL-BOUAGHI", latitude="35.8754", longitude="7.1135", zone_id=3),
+# Wilaya(id=5, name="BATNA", latitude="35.5556", longitude="6.1744", zone_id=4),
+# Wilaya(id=6, name="BEJAIA", latitude="36.7509", longitude="5.0567", zone_id=3),
+# Wilaya(id=7, name="BISKRA", latitude="34.8504", longitude="5.7280", zone_id=4),
+# Wilaya(id=8, name="BECHAR", latitude="31.6238", longitude="-2.2164", zone_id=5),
+# Wilaya(id=9, name="BLIDA", latitude="36.4700", longitude="2.8333", zone_id=2),
+# Wilaya(id=10, name="BOUIRA", latitude="36.3741", longitude="3.9014", zone_id=2),
+# Wilaya(id=11, name="TAMANRASSET", latitude="22.7850", longitude="5.5228", zone_id=5),
+# Wilaya(id=12, name="TEBESSA", latitude="35.4042", longitude="8.1241", zone_id=3),
+# Wilaya(id=13, name="TLEMCEN", latitude="34.8828", longitude="-1.3160", zone_id=1),
+# Wilaya(id=14, name="TIARET", latitude="35.3700", longitude="1.3200", zone_id=1),
+# Wilaya(id=15, name="TIZI-OUZOU", latitude="36.7169", longitude="4.0497", zone_id=3),
+# Wilaya(id=16, name="ALGER", latitude="36.7538", longitude="3.0588", zone_id=2),
+# Wilaya(id=17, name="DJELFA", latitude="34.6667", longitude="3.2500", zone_id=4),
+# Wilaya(id=18, name="JIJEL", latitude="36.8206", longitude="5.7667", zone_id=3),
+# Wilaya(id=19, name="SETIF", latitude="36.1911", longitude="5.4137", zone_id=3),
+# Wilaya(id=20, name="SAIDA", latitude="34.8339", longitude="0.1517", zone_id=1),
+# Wilaya(id=21, name="SKIKDA", latitude="36.8663", longitude="6.9063", zone_id=3),
+# Wilaya(id=22, name="SIDI-BELABBES", latitude="35.1899", longitude="-0.6307", zone_id=1),
+# Wilaya(id=23, name="ANNABA", latitude="36.9000", longitude="7.7667", zone_id=3),
+# Wilaya(id=24, name="GUELMA", latitude="36.4622", longitude="7.4266", zone_id=3),
+# Wilaya(id=25, name="CONSTANTINE", latitude="36.3650", longitude="6.6147", zone_id=3),
+# Wilaya(id=26, name="MOSTAGANEM", latitude="35.9370", longitude="0.0897", zone_id=1),
+# Wilaya(id=27, name="MILA", latitude="36.4547", longitude="6.2494", zone_id=3),
+# Wilaya(id=28, name="ORAN", latitude="35.6954", longitude="-0.6333", zone_id=1),
+# Wilaya(id=29, name="MASCARA", latitude="35.3969", longitude="0.1400", zone_id=1),
+# Wilaya(id=30, name="MEDEA", latitude="36.2644", longitude="2.8889", zone_id=2),
+# Wilaya(id=31, name="SOUK-AHRAS", latitude="36.2889", longitude="7.9511", zone_id=3),
+# Wilaya(id=32, name="TIPAZA", latitude="36.5894", longitude="2.4467", zone_id=2),
+# Wilaya(id=33, name="AIN-DEFLA", latitude="36.2681", longitude="2.8256", zone_id=2),
+# Wilaya(id=34, name="EL-TARF", latitude="36.7573", longitude="8.3105", zone_id=3),
+# Wilaya(id=35, name="NAAMA", latitude="33.2647", longitude="-0.3006", zone_id=5),
+# Wilaya(id=36, name="BISKRA", latitude="34.8504", longitude="5.7280", zone_id=4),
+# Wilaya(id=37, name="LAGHOUAT", latitude="33.8006", longitude="2.8809", zone_id=4),
+# Wilaya(id=38, name="TISSEMSILT", latitude="27.8743", longitude="-0.2939", zone_id=5),
+# Wilaya(id=39, name="EL-BAYADH", latitude="33.2597", longitude="-0.7310", zone_id=5),
+# Wilaya(id=40, name="OUARGLA", latitude="31.9650", longitude="5.3200", zone_id=5),
+# Wilaya(id=41, name="GHARDAIA", latitude="32.4833", longitude="3.6667", zone_id=5),
+# Wilaya(id=42, name="BECHAR", latitude="31.6238", longitude="-2.2164", zone_id=5),
+# Wilaya(id=43, name="EL-OUED", latitude="33.3833", longitude="6.8667", zone_id=5),
+# Wilaya(id=44, name="TINDOUF", latitude="27.6700", longitude="-8.1417", zone_id=5),
+# Wilaya(id=45, name="TIMIMOUN", latitude="29.2500", longitude="0.2500", zone_id=5),
+# Wilaya(id=46, name="OULED DJELLAL", latitude="34.4167", longitude="5.0667", zone_id=5),
+# Wilaya(id=47, name="BORDJ BADJI MOKHTAR", latitude="21.3167", longitude="0.9500", zone_id=5),
+# Wilaya(id=48, name="TAMANRASSET", latitude="22.7850", longitude="5.5228", zone_id=5),
+# Wilaya(id=49, name="ILLIZI", latitude="26.4800", longitude="9.4444", zone_id=5),
+# Wilaya(id=50, name="EL M'GHAÏER", latitude="33.5667", longitude="5.9333", zone_id=5),
+# Wilaya(id=51, name="IN SALAH", latitude="27.2167", longitude="2.4667", zone_id=5),
+# Wilaya(id=52, name="IN GUEZZAM", latitude="19.5667", longitude="5.7667", zone_id=5),
+# Wilaya(id=53, name="DJANET", latitude="24.5500", longitude="9.4833", zone_id=5),
+# Wilaya(id=54, name="EL MENIAÂ", latitude="28.4333", longitude="2.7833", zone_id=5),
+# Wilaya(id=55, name="BORDJ BADJI MOKHTAR", latitude="21.3167", longitude="0.9500", zone_id=5),
+# Wilaya(id=56, name="TAMANRASSET", latitude="22.7850", longitude="5.5228", zone_id=5),
+# Wilaya(id=57, name="BÉNI ABBÈS", latitude="30.1333", longitude="-2.1667", zone_id=5),
+# Wilaya(id=58, name="BORDJ BADJI MOKHTAR", latitude="21.3167", longitude="0.9500", zone_id=5),
+# ]
+
+
+
